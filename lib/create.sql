cfd666f24160b05449e9433ec9fefd6ff2271c77
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 17:55:12 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,16 +3,16 @@ title TEXT,
 category TEXT,
 funding_goal INTEGER,
 start_date TEXT,
-end_date TEXT ) ;
+end_date TEXT );
 
 CREATE TABLE users ( id INTEGER PRIMARY KEY,
 name TEXT,
 age INTEGER,
-) ;
+);
 
 
 CREATE TABLE pledges ( id INTEGER PRIMARY KEY,
   amount NUMERIC,
   user_id INTEGER,
   project_id INTEGER
-) ;
+);
